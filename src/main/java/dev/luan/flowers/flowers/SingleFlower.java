92a0ddf3172c63c6a5fc688a342707a50222e82b
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:02:13 +0200

    Create SingleFlower abstract class
    Introduced SingleFlower abstract class to handle different flower types with specific display and block materials. The class includes constructors to initialize these materials and an abstract method setFlower for setting a flower block.

---
+++ src/main/java/dev/luan/flowers/flowers/SingleFlower.java
@@ -0,0 +1,28 @@
+package dev.luan.flowers.flowers;
+
+import lombok.Getter;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+
+@Getter
+public abstract class SingleFlower {
+
+    private final Material display;
+    private final Material blockMaterial;
+    private final String displayName;
+
+    public SingleFlower(Material display, Material blockMaterial, String displayName) {
+        this.display = display;
+        this.blockMaterial = blockMaterial;
+        this.displayName = displayName;
+    }
+
+    public SingleFlower(Material blockMaterial, String displayName) {
+        this.display = blockMaterial;
+        this.blockMaterial = blockMaterial;
+        this.displayName = displayName;
+    }
+
+    public abstract void setFlower(Block block);
+}
