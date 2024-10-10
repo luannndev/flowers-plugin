1bc2dd2174eaa3f19465db90b5e6c7a20e6626c3
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:24:50 +0200

    Add DoubleFlower class to support bisected flowers
    Introduced a new DoubleFlower class extending SingleFlower to handle bisected flower types. This class includes the ability to set the flower type on a block and manage its half property using Bukkit's Bisected interface.

---
+++ src/main/java/dev/luan/flowers/flowers/placable/DoubleFlower.java
@@ -0,0 +1,33 @@
+package dev.luan.flowers.flowers.placable;
+
+import dev.luan.flowers.flowers.SingleFlower;
+import lombok.Getter;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Bisected;
+
+@Getter
+public final class DoubleFlower extends SingleFlower {
+
+    private final Bisected.Half half;
+
+    public DoubleFlower(Material display, Material blockMaterial, String displayName, Bisected.Half half) {
+        super(display, blockMaterial, displayName);
+        this.half = half;
+    }
+    public DoubleFlower(Material blockMaterial, String displayName, Bisected.Half half) {
+        super(blockMaterial, displayName);
+        this.half = half;
+    }
+
+    @Override
+    public void setFlower(Block block) {
+        block.setType(this.getBlockMaterial());
+
+        if (block.getBlockData() instanceof Bisected bisected) {
+            bisected.setHalf(this.half);
+            block.setBlockData(bisected);
+        }
+    }
+}
