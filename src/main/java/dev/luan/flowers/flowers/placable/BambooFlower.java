b78bfd0661a2fa9e2e6b5ffbc23621af22afa6fc
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:22:42 +0200

    Add BambooFlower class to handle bamboo-specific block placement
    This commit introduces the BambooFlower class extending AgebleFlower to manage the placement of bamboo blocks. It includes functionality to set leaves type and age for a bamboo block using the provided parameters. The class ensures proper bamboo block configuration during the placement.

---
+++ src/main/java/dev/luan/flowers/flowers/placable/BambooFlower.java
@@ -0,0 +1,34 @@
+package dev.luan.flowers.flowers.placable;
+
+import dev.luan.flowers.flowers.AgebleFlower;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Bamboo;
+
+@Getter
+public final class BambooFlower extends AgebleFlower {
+
+    private final Bamboo.Leaves leaves;
+
+    public BambooFlower(Material display, Material blockMaterial, String displayName, int age, Bamboo.Leaves leaves) {
+        super(display, blockMaterial, displayName, age);
+        this.leaves = leaves;
+    }
+
+    public BambooFlower(Material blockMaterial, String displayName, int age, Bamboo.Leaves leaves) {
+        super(blockMaterial, displayName, age);
+        this.leaves = leaves;
+    }
+
+    @Override
+    public void setFlower(Block block) {
+        block.setType(this.getBlockMaterial());
+
+        if (block.getBlockData() instanceof Bamboo bamboo) {
+            bamboo.setLeaves(this.leaves);
+            bamboo.setAge(this.getAge());
+            block.setBlockData(bamboo);
+        }
+    }
+}
