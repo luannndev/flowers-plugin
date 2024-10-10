6206cb72b1fbdbec52f0a0efaeebe2bac8e3fc13
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 22:58:42 +0200

    Add ItemBuilder utility class for item stack manipulation
    This commit introduces the ItemBuilder class which simplifies the creation and customization of ItemStack objects in Bukkit. The class supports setting item names and lore using both Component and LanguageManager keys, enhancing code readability and maintainability.

---
+++ src/main/java/dev/luan/flowers/utils/ItemBuilder.java
@@ -0,0 +1,57 @@
+package dev.luan.flowers.utils;
+
+import dev.luan.flowers.managers.LanguageManager;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+public final class ItemBuilder {
+
+    private final ItemStack itemStack;
+
+    public ItemBuilder(Material material) {
+        this.itemStack = new ItemStack(material);
+    }
+
+    public ItemBuilder setName(Component component) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(component);
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemBuilder setName(String key) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(LanguageManager.getComponent(key));
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemBuilder setName(String key, String replace, String replacement) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(LanguageManager.getComponent(key, replace, replacement));
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemBuilder setLore(List<Component> components) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.lore(components);
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemBuilder setLore(String key) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.lore(LanguageManager.getComponents(key));
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public ItemStack build() {
+        return itemStack;
+    }
+}
