e9b45b9544362e08f01b9c96a86b5f36ecc23e11
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:02:21 +0200

    Add WaterloggableFlower class for waterlogged flower support
    Introduces the WaterloggableFlower class extending SingleFlower to handle waterlogged state of flowers. The class includes constructors to set initial flower properties and waterlogged status.

---
+++ src/main/java/dev/luan/flowers/flowers/WaterloggableFlower.java
@@ -0,0 +1,20 @@
+package dev.luan.flowers.flowers;
+
+import lombok.Getter;
+
+import org.bukkit.Material;
+
+@Getter
+public abstract class WaterloggableFlower extends SingleFlower {
+
+    private final boolean isWaterlogged;
+
+    public WaterloggableFlower(Material display, Material blockMaterial, String displayName, boolean isWaterlogged) {
+        super(display, blockMaterial, displayName);
+        this.isWaterlogged = isWaterlogged;
+    }
+    public WaterloggableFlower(Material blockMaterial, String displayName, boolean isWaterlogged) {
+        super(blockMaterial, displayName);
+        this.isWaterlogged = isWaterlogged;
+    }
+}
