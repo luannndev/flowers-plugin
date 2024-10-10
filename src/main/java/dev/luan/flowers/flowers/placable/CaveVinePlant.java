cea99c2da4b45225051fafe740d92ddb2c96fa65
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:24:25 +0200

    Add CaveVinePlant class with age and berry properties
    Introduced a new CaveVinePlant class that extends AgebleFlower. This class includes properties for age and the presence of berries, and overrides the setFlower method to properly configure block data for Cave Vines.

---
+++ src/main/java/dev/luan/flowers/flowers/placable/CaveVinePlant.java
@@ -0,0 +1,33 @@
+package dev.luan.flowers.flowers.placable;
+
+import dev.luan.flowers.flowers.AgebleFlower;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.CaveVines;
+
+@Getter
+public final class CaveVinePlant extends AgebleFlower {
+    private final boolean hasBarries;
+
+    public CaveVinePlant(Material display, Material blockMaterial, String displayName, int age, boolean hasBerries) {
+        super(display, blockMaterial, displayName, age);
+        this.hasBarries = hasBerries;
+    }
+
+    public CaveVinePlant(Material blockMaterial, String displayName, int age, boolean hasBerries) {
+        super(blockMaterial, displayName, age);
+        this.hasBarries = hasBerries;
+    }
+
+    @Override
+    public void setFlower(Block block) {
+        block.setType(this.getBlockMaterial());
+
+        if (block.getBlockData() instanceof CaveVines caveVines) {
+            caveVines.setBerries(hasBarries);
+            caveVines.setAge(getAge());
+            block.setBlockData(caveVines);
+        }
+    }
+}
