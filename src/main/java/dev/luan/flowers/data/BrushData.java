e88bd83f5e744d05ebbde9838897770238bd9bd3
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-10 23:22:48 +0200

    Add BrushData record
    Introduce BrushData.java to encapsulate flower group data, radius, air randomizer, and global masks for organizing brush-related functionality. This prepares the codebase for future enhancements and more structured data management.

---
+++ src/main/java/dev/luan/flowers/data/BrushData.java
@@ -0,0 +1,10 @@
+package dev.luan.flowers.data;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
+public record BrushData(List<FlowerGroupData> flowerGroupData, int radius, float airRandomizer,
+                        List<Material> globalMasks) {}
